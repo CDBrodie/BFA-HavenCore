3412e7a200a7b5de943513a1a58b02454372ffc9
Denislav\Hex <t06753699@gmail.com>
2026-08-01 15:07:36 +0300

    fix: add safeguards to some SQL updates

--- sql/updates/world/2026_07_9_00.sql
+++ sql/updates/world/2026_07_9_00.sql
@@ -1,9 +1,11 @@
-INSERT INTO quest_poi 
-(QuestID, BlobIndex, Idx1, ObjectiveIndex, QuestObjectiveID, QuestObjectID, MapID, UiMapID, Priority, Flags, WorldEffectID, PlayerConditionID, SpawnTrackingID, AlwaysAllowMergingBlobs)
+DELETE FROM `quest_poi` WHERE `QuestID` = 39049 AND `Idx1` = 0;
+INSERT INTO `quest_poi` 
+(`QuestID`, `BlobIndex`, `Idx1`, `ObjectiveIndex`, `QuestObjectiveID`, `QuestObjectID`, `MapID`, `UiMapID`, `Priority`, `Flags`, `WorldEffectID`, `PlayerConditionID`, `SpawnTrackingID`, `AlwaysAllowMergingBlobs`)
 VALUES 
 (39049, 0, 0, 0, 279707, 105946, 1481, 672, 0, 0, 0, 0, 0, 0);
 
-INSERT INTO quest_poi_points 
-(QuestID, Idx1, Idx2, X, Y)
+DELETE FROM `quest_poi_points` WHERE `QuestID` = 39049 AND `Idx1` = 0 AND `Idx2` = 0;
+INSERT INTO `quest_poi_points` 
+(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`)
 VALUES 
 (39049, 0, 0, 593, 2433);
