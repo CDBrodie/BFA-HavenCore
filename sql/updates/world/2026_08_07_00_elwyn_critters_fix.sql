bdd848be74a2aa73ec53e6eaa62e804155d191fc
Denislav\Hex <t06753699@gmail.com>
2026-08-08 02:17:39 +0300

    DB: Fix Elwynn Forest Critters (fixes #115)

---
+++ sql/updates/world/2026_08_07_00_elwyn_critters_fix.sql
@@ -0,0 +1,15 @@
+UPDATE `creature_template_scaling`
+SET 
+  `LevelScalingMin` = 1,
+  `LevelScalingMax` = 1
+WHERE `Entry` = 62954;
+
+UPDATE `creature_template_scaling`
+SET 
+  `LevelScalingMin` = 1,
+  `LevelScalingMax` = 1
+WHERE `Entry` = 1412;
+
+UPDATE creature_template
+SET HealthModifier = 0.03225806
+WHERE entry = 721;
