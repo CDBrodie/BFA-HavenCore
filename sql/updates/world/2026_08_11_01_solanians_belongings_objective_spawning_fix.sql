de100d770a1b8a1e89446fe97fec8f3bbdab8002
vancho.mladenov@gmail.com <vancho.mladenov@gmail.com>
2026-08-11 13:45:25 +0300

    Solanian's Belongings - Objects spawn time Fix

---
+++ sql/updates/world/2026_08_11_01_solanians_belongings_objective_spawning_fix.sql
@@ -0,0 +1,12 @@
+-- Journal
+UPDATE gameobject 
+SET spawntimesecs = 60 
+WHERE guid = 20007
+-- Scroll
+UPDATE gameobject 
+SET spawntimesecs = 60 
+WHERE guid = 20008
+-- Orb
+UPDATE gameobject 
+SET spawntimesecs = 60 
+WHERE guid = 19565
