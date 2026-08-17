889d9cc01a9e3b3a7ef3f4b3e22200131831944f
Denislav\Hex <132233897+Hextv@users.noreply.github.com>
2026-08-17 11:13:42 +0300

    DB: Fix Stoneform sql syntax

--- sql/updates/hotfixes/2026_08_16_00_stoneform.sql
+++ sql/updates/hotfixes/2026_08_16_00_stoneform.sql
@@ -1,1 +1,1 @@
-INSERT INTO `hotfixes`.`spell_cooldowns` (`ID`, `CategoryRecoveryTime`, `RecoveryTime`, `VerifiedBuild`) VALUES ('1285', '30000', '120000', '20594'); 
+INSERT INTO `spell_cooldowns` (`ID`, `CategoryRecoveryTime`, `RecoveryTime`, `VerifiedBuild`) VALUES (1285, 30000, 120000, 20594);
